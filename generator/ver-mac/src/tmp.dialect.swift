29c10e297843dcec4092d101731173dca38dcfaf
Михаил Капелько <kornerr@gmail.com>
2026-08-28 14:50:51 +0300

    stub CLIContext

--- generator/ver-mac/src/tmp.dialect.swift
+++ generator/ver-mac/src/tmp.dialect.swift
@@ -8,5 +8,35 @@ func cliSet(
     CLIComponent.singleton!.ctrl.set(key, value)
 }
 
+struct CLIContext: DialectContext {
+    var didLaunch = false
+    var didSetup = false
 
+    var recentField = ""
 
+    func field<T>(_ name: String) -> T {
+        if (name == "didLaunch") {
+            return didLaunch as! T
+        } else if (name == "didSetup") {
+            return didSetup as! T
+        }
+
+        return "unknown-field-name" as! T
+    }
+
+    mutating func setField(
+        _ name: String,
+        _ value: Any
+    ) {
+        if (name == "didLaunch") {
+            didLaunch = value as! Bool
+        } else if (name == "didSetup") {
+            didSetup = value as! Bool
+        }
+    }
+}
+
+struct F {
+    static let didLaunch = "didLaunch"
+    static let didSetup = "didSetup"
+}
