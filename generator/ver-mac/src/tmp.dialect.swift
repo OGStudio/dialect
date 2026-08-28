7254e3c02a0d4288fbf38b47826caf9db45d0315
Михаил Капелько <kornerr@gmail.com>
2026-08-28 14:54:43 +0300

    fill in the fields

--- generator/ver-mac/src/tmp.dialect.swift
+++ generator/ver-mac/src/tmp.dialect.swift
@@ -9,16 +9,34 @@ func cliSet(
 }
 
 struct CLIContext: DialectContext {
+    var arguments = [String]()
     var didLaunch = false
     var didSetup = false
+    var input = ""
+    var inputFileName = ""
+    var inputFileNameContents = ""
+    var readFile = false
+    var stdin = ""
 
     var recentField = ""
 
     func field<T>(_ name: String) -> T {
-        if (name == "didLaunch") {
+        if (name == "arguments") {
+            return arguments as! T
+        } else if (name == "didLaunch") {
             return didLaunch as! T
         } else if (name == "didSetup") {
             return didSetup as! T
+        } else if (name == "input") {
+            return input as! T
+        } else if (name == "inputFileName") {
+            return inputFileName as! T
+        } else if (name == "inputFileNameContents") {
+            return inputFileNameContents as! T
+        } else if (name == "readFile") {
+            return readFile as! T
+        } else if (name == "stdin") {
+            return stdin as! T
         }
 
         return "unknown-field-name" as! T
@@ -28,15 +46,33 @@ struct CLIContext: DialectContext {
         _ name: String,
         _ value: Any
     ) {
-        if (name == "didLaunch") {
+        if (name == "arguments") {
+            arguments = value as! [String]
+        } else if (name == "didLaunch") {
             didLaunch = value as! Bool
         } else if (name == "didSetup") {
             didSetup = value as! Bool
+        } else if (name == "input") {
+            input = value as! String
+        } else if (name == "inputFileName") {
+            inputFileName = value as! String
+        } else if (name == "inputFileNameContents") {
+            inputFileNameContents = value as! String
+        } else if (name == "readFile") {
+            readFile = value as! Bool
+        } else if (name == "stdin") {
+            stdin = value as! String
         }
     }
 }
 
 struct F {
+    static let arguments = "arguments"
     static let didLaunch = "didLaunch"
     static let didSetup = "didSetup"
+    static let input = "input"
+    static let inputFileName = "inputFileName"
+    static let inputFileNameContents = "inputFileNameContents"
+    static let readFile = "readFile"
+    static let stdin = "stdin"
 }
