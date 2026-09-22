65268e21e15e5b548bea40266a7f19b73d218544
Михаил Капелько <kornerr@gmail.com>
2026-09-22 16:04:36 +0300

    close

--- example/ver-mac/src/tmp.dialect.swift
+++ example/ver-mac/src/tmp.dialect.swift
@@ -82,7 +82,9 @@ func rootSet(
 // Root oneliners
 
 func rootRegisterEffects(_ ctrl: DialectController) {
+    RootVM.shared?.countText = "todo"
     let _: RootContext? = registerOneliners(ctrl, [
-        F.countText, { (c: RootContext) in RootVM.shared?.countText = c.countText },
+        F.countText, { (c: RootContext) in print(c.countText) },
+        //F.countText, { (c: RootContext) in RootVM.shared?.countText = c.countText },
     ])
 }
